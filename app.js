a938bcdc841387cd132df3ad44c86e1a0b56f547
eminalpernl <eminalpernl@gmail.com>
2023-01-08 13:54:50 +0100

    stylization initial

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 'use strict';
+let slideIndex = 1;
 const body = document.getElementsByTagName('body');
 const API = 'https://api.nasa.gov/EPIC/api/natural/date/';
+const imgApi = 'https://api.nasa.gov/EPIC/archive/natural/';
 const dateInput = document.getElementById("date-input");
 const button = document.getElementById('button');
 const listWrap = document.createElement('div');
-const exampleQuery = 'https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq';
-                      //https://api.nasa.gov/EPIC/archive/natural/2016-01-05/png/epic_RGB_20160105111530.png?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq
-                      //https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq
-                      //https://api.nasa.gov/EPIC/archive/natural/2019/02/07/png/epic_1b_20190207023358.png?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq
-                      //https://api.nasa.gov/EPIC/archive/natural/2019-02-07/png/epic_1b_20190207023358.png?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq
-                      //https://api.nasa.gov/EPIC/archive/natural/2019/02/07/png/epic_1b_20190207023358.png?api_key=CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq
-
-const imgApi = 'https://api.nasa.gov/EPIC/archive/natural/';
 const API_KEY = 'CR6PmDZLb5fTpRM9XpBhg7Ptm55c56rv8q5xfzxq';
-//const imageUrl ='https://epic.gsfc.nasa.gov/api/natural/date/{$year}-{$month}-{$day}'
 
 async function fetchData(url) {
   const response = await fetch(url);
@@ -29,6 +22,7 @@ async function fetchImageDate2() {
     listWrap.innerHTML = "";
     const dateList = document.createElement("select");
     dateList.className='select-box';
+    listWrap.className= 'list-wrap';
     listWrap.appendChild(dateList);
     body[0].appendChild(listWrap);
     const URL = `${API}${dateInput.value}?api_key=${API_KEY}`;
@@ -44,94 +38,108 @@ async function fetchImageDate2() {
     const getValueOfSelection = await fetchImage();
 };
 
+
+
+// async function fetchImage() {
+//   const imgWrap = document.createElement('div');
+//   imgWrap.className = 'img-wrap';
+//   const earthImg = document.createElement('img');
+//   const selected = document.getElementsByClassName('select-box');
+//   const inputValue = (dateInput.value).split('-').join('/');
+//   selected[0].addEventListener('change', async (event) => {
+//     imgWrap.innerHTML='';
+//     const grabImageId = event.target.value;
+//     const imgUrl = `${imgApi}${inputValue}/png/${grabImageId}.png?api_key=${API_KEY}`;
+//     console.log(grabImageId);
+//     imgWrap.appendChild(earthImg);
+//     body[0].appendChild(imgWrap);
+//     console.log(imgUrl);
+//     earthImg.className = 'earth-image';
+//     earthImg.src = imgUrl;
+//     earthImg.alt = 'Earth';
+//   });
+// }
+
 async function fetchImage() {
+  const imgWrap = document.createElement('div');
+  imgWrap.className = 'img-wrap';
+  const earthImg = document.createElement('img');
   const selected = document.getElementsByClassName('select-box');
   const inputValue = (dateInput.value).split('-').join('/');
   selected[0].addEventListener('change', async (event) => {
+    imgWrap.innerHTML='';
     const grabImageId = event.target.value;
     const imgUrl = `${imgApi}${inputValue}/png/${grabImageId}.png?api_key=${API_KEY}`;
     console.log(grabImageId);
-    const imgWrap = document.createElement('div');
-    const earthImg = document.createElement('img');
     imgWrap.appendChild(earthImg);
     body[0].appendChild(imgWrap);
     console.log(imgUrl);
-
-    // const imgFetch = await (await fetch(grabApi)).json();
-    // console.log(imgFetch);
-    // earthImg.src = imgFetch.sprites.other.home.front_default;
-    // earthImg.alt = imgFetch.forms[0].name;
+    earthImg.className = 'earth-image';
+    earthImg.src = imgUrl;
+    earthImg.alt = 'Earth';
   });
+
+  const printIdList = await makeImageSlide();
+
 }
 
-// async function fetchImage() {
-//   const selected = document.getElementById('select-box');
-//   const 
-//   selected.addEventListener('change', async (event) => {
-//     const grabApi = event.target.value;
-//     const imgWrap = document.createElement('div');
-//     const earthImg = document.createElement('img');
-//     imgWrap.appendChild(earthImg);
-//     body[0].appendChild(imgWrap);
+async function makeImageSlide(){
+  const slideNexButton = document.createElement('a');
+  slideNexButton.className = 'next';
+  slideNexButton.setAttribute('onclick', "plusSlides(1)");
+  slideNexButton.innerHTML = '&#10095;';
+  const slidePrevButton = document.createElement('a');
+  slidePrevButton.className = 'prev';
+  slidePrevButton.setAttribute('onclick', "plusSlides(-1)");
+  slidePrevButton.innerHTML = '&#10094;';
+  const inputValue = (dateInput.value).split('-').join('/');
+  const ImgIdList = document.getElementsByTagName('option');
+  const slideWrap = document.createElement('div');
+  slideWrap.className = 'slide-wrap';
+  
+  for (let index = 0; index < ImgIdList.length; index++) {
+    const singleImageDiv = document.createElement('div');
+    singleImageDiv.className = 'single-image-div';
+    const imgOrder = document.createElement('div');
+    imgOrder.className = 'number-text';
+    const img = document.createElement('img');
+    const captionDiv = document.createElement('div');
+    captionDiv.className='text';
+    captionDiv.innerText=`caption ${index+1}`;
+    const imgUrl = `${imgApi}${inputValue}/png/${ImgIdList[index].value}.png?api_key=${API_KEY}`;
+    img.alt = 'earth';
+    img.src = imgUrl;
+    img.classList='img-on-slides';
+    singleImageDiv.appendChild(imgOrder);
+    singleImageDiv.appendChild(img);
+    singleImageDiv.appendChild(captionDiv);
+    slideWrap.appendChild(singleImageDiv);
+    body[0].appendChild(slideWrap);
+  }
+  
+  slideWrap.appendChild(slidePrevButton);
+  slideWrap.appendChild(slideNexButton);
 
-//     const imgFetch = await (await fetch(grabApi)).json();
-//     console.log(imgFetch);
-//     pokeImg.src = imgFetch.sprites.other.home.front_default;
-//     pokeImg.alt = imgFetch.forms[0].name;
-//   });
-// }
+  const runSlides = await showSlides(slideIndex);
+}
+
+async function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
 
-// async function fetchImageDate(url, query) {
-//   const urlDate = `${url}${query}`;
-//   const dateInput = document.getElementById('date-input');
-//   const dateList = document.createElement("select");
-//   const submit = document.getElementById('submit');
-//   body[0].appendChild(dateList);
-//   submit.addEventListener('click', async (event) => {
-//     const inputValue = dateInput.target.value;
-//         // const pokeImg = document.getElementById('poke-img');
-//         const imgFetch = await (await fetch(grabDate)).json();
-//         console.log(inputValue);
-//         // pokeImg.src = imgFetch.sprites.other.home.front_default;
-//         // pokeImg.alt = imgFetch.forms[0].name;
-//       });
-//   //const selection = document.getElementById('date-selection'); 
-//   const jsonData = await fetchData(urlDate);
-//   console.log(jsonData);
-//   for (let index = 0; index < jsonData.length; index++) {
-//     const element = document.createElement('option');
-//     element.value = jsonData[index].date;
-//     element.textContent = jsonData[index].date;
-//    // selection.appendChild(element);
-//     dateList.appendChild(element);
-//   }
-// //;
-// }
+async function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("single-image-div");
+  //let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
 
-// fetchImageDate(URL, '2022-01-01');
-
-// async function fetchImage() {
-//   const selected = document.getElementById('poke-list');
-//   selected.addEventListener('change', async (event) => {
-//     const grabApi = event.target.value;
-//     const pokeImg = document.getElementById('poke-img');
-//     const imgFetch = await (await fetch(grabApi)).json();
-//     console.log(imgFetch);
-//     pokeImg.src = imgFetch.sprites.other.home.front_default;
-//     pokeImg.alt = imgFetch.forms[0].name;
-//   });
-// }
-
-// async function main() {
-//   try {
-//     await fetchData(URL);
-//     await fetchAndPopulatePokemons(URL);
-//     await fetchImage();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 
 async function main() {
     const submit = document.getElementById('button');
